53eaa000722bb2498f6600bd9028c062074fb913
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-11-14 15:11:51 -0300

    Adicionar a paleta de cores, o README, e arquivos exemplos

---
+++ AcademyKit/Sources/AcademyKit/Atoms/ExampleAtom.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Gabriel Vicentin Negro on 14/11/23.
+//
+
+import SwiftUI
+
+public struct ExampleAtom: View {
+    public init(){}
+    public var body: some View {
+        Text("Seu átomo aqui!")
+    }
+}
+
+#Preview {
+    ExampleAtom()
+}
---
+++ AcademyKit/Sources/AcademyKit/Components/ExampleComponents.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Gabriel Vicentin Negro on 14/11/23.
+//
+
+import SwiftUI
+
+public struct SwiftUIView: View {
+    public init(){}
+    public var body: some View {
+        Text("Seu componente aqui!")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
