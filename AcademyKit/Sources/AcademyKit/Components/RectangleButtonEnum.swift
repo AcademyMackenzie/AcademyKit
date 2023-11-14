64843459510cc7367fb571bf4be82e9e82a2c2ec
Thiago Leandro Liporace <85422792+thiliporace@users.noreply.github.com>
2023-11-14 16:46:30 -0300

    adicionar codigo do botão "RectangleButton"

---
+++ AcademyKit/Sources/AcademyKit/Components/RectangleButtonEnum.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Thiago Liporace on 14/11/23.
+//
+
+import Foundation
+
+enum RectangleButtonType {
+    case primary
+    case tertiary
+}
+
+
---
+++ AcademyKit/Sources/AcademyKit/Components/RectangleButtonNoFill.swift
@@ -0,0 +1,56 @@
+//
+//  RectangleButton.swift
+//  RectangleButton
+//
+//  Created by Thiago Liporace on 14/11/23.
+//
+
+import SwiftUI
+
+struct RectangleButtonNoFill: ButtonStyle {
+    
+    var hasIcon: Bool
+    var isRectangleButtonDisabled: Bool
+    var buttonType: RectangleButtonType
+    
+    func makeBody(configuration: Configuration) -> some View {
+        HStack(spacing: 15){
+            if hasIcon{
+                Image(systemName: "magnifyingglass")
+                    .font(.system(size: 18))
+                    .foregroundStyle(Color.white)
+            }
+            
+            configuration.label
+            
+                .font(.custom("Cygre", size: 17))
+                .bold()
+                .foregroundStyle(Color.white)
+            
+        }
+        .padding(.vertical, hasIcon ? 16 : 16)
+        .padding(.horizontal, hasIcon ? 29 : 40)
+        
+        //.background(isRectangleButtonDisabled ? Color.academyGra2 : buttonType == .primary ? (configuration.isPressed ? Color.academyDarkBlue : Color.academyBlue)  : (configuration.isPressed ? Color.academyDarkPurple : Color.academyPurple))
+        .overlay(
+            RoundedRectangle(cornerRadius: 8)
+                .stroke(isRectangleButtonDisabled ? Color.academyGra2 : buttonType == .primary ? (configuration.isPressed ? Color.academyDarkBlue : Color.academyBlue)  : (configuration.isPressed ? Color.academyDarkPurple : Color.academyPurple), lineWidth: 1)
+        )
+    
+       
+    }
+}
+
+#Preview {
+    ZStack{
+        
+        Color.black
+        
+        Button("Secondary"){
+            
+        }
+        .buttonStyle(RectangleButtonNoFill(hasIcon: true, isRectangleButtonDisabled: false, buttonType: .primary))
+    }
+    
+        
+}
