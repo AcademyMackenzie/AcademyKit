5cdbd9d44c7e0981f3c460323530da3b027a3b58
Carreirinha <carreirarafael3@gmail.com>
2023-11-16 17:13:35 -0300

    Update do componente TextButton, Adicionado a fonte correta e funcionabilidade corrijida

--- AcademyKit/Sources/AcademyKit/Components/TextButton.swift
+++ AcademyKit/Sources/AcademyKit/Components/TextButton.swift
@@ -12,33 +12,30 @@ import SwiftUI
 ///  - Parameters:
 ///     - colorScheme: Esta variável contém informações sobre o modo claro e o modo escuro do dispositivo
 ///     - text: Esta variável é o texto passado para o botão
-///     - action: Esta variável é a ação que o botão irá realizar
 ///
 public struct TextButton: View {
     ///Esta variável contém informações sobre o modo claro e o modo escuro do dispositivo
     @Environment(\.colorScheme) var colorScheme
+    @Environment(\.isEnabled) var isEnabled
     ///Esta variável é o texto passado para o botão
     var text: String
-    ///Esta variável é a ação que o botão irá realizar
-    var action: () -> Void
     
     ///Inicializa as variaveis da Struct
-    public init(_ text: String, action: @escaping () -> Void) {
+    public init(_ text: String) {
         self.text = text
-        self.action = action
     }
     
     public var body: some View {
-        Button(action:{
-                action()
-        }, label: {
-            Text(text)
-                .font(.system(size: 18))
+   
+        Text(text)
+                .font(.custom("Cygre-Bold", size: 18))
                 .bold()
-                .underline()
-                
-        }).foregroundStyle(colorScheme == .dark ? Color.academyWhite : Color.academyBlack)
-        
+                .underline(isEnabled)
+                .foregroundStyle(colorScheme == .dark ? isEnabled ?  Color.academyWhite : Color.academyGray1 : isEnabled ?  Color.academyBlack : Color.academyGray1 )
             
     }
+}
+
+#Preview {
+    TextButton("Cancelar")
 }
