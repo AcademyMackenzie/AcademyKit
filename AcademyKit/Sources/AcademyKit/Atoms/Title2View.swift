ce88774af8eb88102522ff44674422f96a1a4b9a
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-11-16 15:00:44 -0300

    Adicionar a headline no pacote, e sua documentação, e arrumar a documentação de todas as outras Views adicionadas ate agora

--- AcademyKit/Sources/AcademyKit/Atoms/Title2View.swift
+++ AcademyKit/Sources/AcademyKit/Atoms/Title2View.swift
@@ -13,13 +13,13 @@ public struct Title2View: View {
     
     /// Variável que recebe o texto que deseja mostrar como parâmetro
     public var text: String
-    /// Variável que recebe o texto que deseja mostrar como parâmetro
+    /// Variável que recebe o peso que deseja mostrar como parâmetro
     public var weight: Weight
     
     /// Inicializador da View
     /// - Parameters:
     ///   - text: Variável que recebe o texto que deseja mostrar como parâmetro
-    ///   - weight: Variável que recebe o texto que deseja mostrar como parâmetro
+    ///   - weight: Variável que recebe o peso que deseja mostrar como parâmetro
     public init(text: String, weight: Weight){
         self.text = text
         self.weight = weight
