ce6eeaedfd89434614522aec16c4b2ba9c2604a4
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-11-16 14:39:50 -0300

    Criar a Title1View, e sua documentação

---
+++ AcademyKit/Sources/AcademyKit/Atoms/Title1View.swift
@@ -0,0 +1,38 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Gabriel Vicentin Negro on 16/11/23.
+//
+
+import SwiftUI
+
+
+///View que mostra um texto de título do nosso pacote
+public struct Title1View: View {
+    
+    /// Variável que recebe o texto que deseja mostrar como parâmetro
+    public var text: String
+    /// Variável que recebe o texto que deseja mostrar como parâmetro
+    public var weight: Weight
+    
+    /// Inicializador da View
+    /// - Parameters:
+    ///   - text: Variável que recebe o texto que deseja mostrar como parâmetro
+    ///   - weight: Variável que recebe o texto que deseja mostrar como parâmetro
+    public init(text: String, weight: Weight){
+        self.text = text
+        self.weight = weight
+    }
+    
+    ///  Corpo da View
+    public var body: some View {
+        Text(text)
+            .font(.custom(weight == .regularWeight ? "Cygre-Regular" : "Cygre-Bold", size: 28))
+    }
+}
+
+
+#Preview {
+    Title1View(text: "Title1",weight: .boldWeight)
+}
